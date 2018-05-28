0700e15ec0fd9b2cf0fcc9f251c9d12f8f5163cf
openui <bobo_8318@163.com>
2018-05-28 15:13:06 +0800

    new web site

---
+++ content/test.md
@@ -0,0 +1,10 @@
+﻿title: 下载吧第一篇文章测试
+date: 2018-05-28
+Category: Python
+Tags: pelican, publishing
+Slug: my-super-post
+Authors: openui
+Summary: idown测试文章
+
+##第一篇测试
+- 欢迎来到下载吧，欢迎下载你需要的资源
---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'openui'
+SITENAME = '下载吧'
+SITEURL = '/'
+
+PATH = 'content'
+
+TIME_ZONE = 'Asia/Shanghai'#时区设置
+
+DEFAULT_LANG = 'Chinese (Simplified)'
+THEME = "alchemy"
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('openui', 'http://www.openui.cn'),
+         ('imba', 'http://www.imba.pub'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
